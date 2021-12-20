4730c288ed863f080f6db3ca33ada28281644423
jcmtallon <juanca_mt@hotmail.com>
2021-12-20 22:05:52 +0100

    feat: misc changes

---
+++ packages/eslint-config-glamping-base/rules/airbnb-rules.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+   baseBestPracticesRules: require('eslint-config-airbnb-base/rules/best-practices'),
+   baseErrorsRules: require('eslint-config-airbnb-base/rules/errors'),
+   baseES6Rules: require('eslint-config-airbnb-base/rules/es6'),
+   baseImportsRules: require('eslint-config-airbnb-base/rules/imports'),
+   baseStyleRules: require('eslint-config-airbnb-base/rules/style'),
+   baseVariablesRules: require('eslint-config-airbnb-base/rules/variables'),
+}
+
+
+
+
+
+
+
