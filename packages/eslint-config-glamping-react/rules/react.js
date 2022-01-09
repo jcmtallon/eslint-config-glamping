0306b03afeb624023c77203b1f58b06fc19377b3
jcmtallon <juanca_mt@hotmail.com>
2022-01-09 13:47:24 +0100

    fix: dependency path

--- packages/eslint-config-glamping-react/rules/react.js
+++ packages/eslint-config-glamping-react/rules/react.js
@@ -1,5 +1,5 @@
 const assign = require('object.assign');
-const baseStyleRules = require('eslint-config-airbnb-base/rules/style').rules;
+const baseStyleRules = require('@jcmtallon/eslint-config-glamping-base/rules/style').rules;
 
 const dangleRules = baseStyleRules['no-underscore-dangle'];
 
