0d36bb2d9238a57c900fd7801ef5d8c1d24d7af6
jcmtallon <juanca_mt@hotmail.com>
2022-01-09 12:18:57 +0100

    chore: misc improvements

--- packages/eslint-config-glamping-react/index.js
+++ packages/eslint-config-glamping-react/index.js
@@ -3,6 +3,5 @@ module.exports = {
     '@jcmtallon/eslint-config-glamping-base',
     './rules/react',
     './rules/react-a11y',
-  ].map(require.resolve),
-  rules: {},
+  ].map(require.resolve)
 }
