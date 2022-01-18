e25f44c4ed8fedcb9cecd71fab597fae1e3dd4d9
jcmtallon <juanca_mt@hotmail.com>
2022-01-18 18:52:45 +0100

    feat: tweak rules

--- packages/eslint-config-glamping-base/rules/imports.js
+++ packages/eslint-config-glamping-base/rules/imports.js
@@ -120,7 +120,36 @@ module.exports = {
     // ensure absolute imports are above relative imports and that unassigned imports are ignored
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
     // TODO: enforce a stricter convention in module import order?
-    'import/order': ['error', { groups: [['builtin', 'external', 'internal']] }],
+    'import/order': ['error', {
+      groups: [
+        'builtin',
+        'external',
+        'internal',
+        'parent',
+        'sibling',
+        'index',
+        'object',
+        // We don't include 'type' so types imported via 'import type' are placed
+        // together with other imports from the same depth level.
+      ],
+      alphabetize: { order: 'asc' },
+      'newlines-between': 'always',
+      pathGroups: [
+        {
+          pattern: '@jcmtallon/**',
+          group: 'external',
+          position: 'after',
+        },
+        {
+          pattern: './**.styles',
+          group: 'type',
+          position: 'after',
+        },
+      ],
+      // pathGroupsExcludedImportTypes defaults to ["builtin", "external"], which can
+      // cause confusion when setting up pathGroups, so we reset the value
+      pathGroupsExcludedImportTypes: [],
+    }],
 
     // Require a newline after the last import/require in a group
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
